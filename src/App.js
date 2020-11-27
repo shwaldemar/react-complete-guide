cf5a04f32602f98130df4cb61d6789fd1b17ee94
Simon <shwaldemar@gmail.com>
2020-11-27 10:46:56 +0000

    changed from class and component to function based state management.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person.js'
 
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState] = useState({
     persons : [
       {name: "Simon", age: 48},
       {name: "Tarka", age: 1.5},
       {name: "Poacher", age: 45}
     ], otherState: 'some other value'
-  }
-  switchNameHandler = () => {
+  });
+
+  const switchNameHandler = () => {
     //console.log("This button was clicked")
-    this.setState({
+    setPersonsState({
       persons: [
         {name: "Simon Hall", age: 48},
         {name: "Tarka", age: 1.5},
@@ -21,20 +22,27 @@ class App extends Component {
       ]
     })
   }
-  render() {
+
+
     return (
       <div className="App">
           <h1 >Hi I'm a React App</h1>
           <p>This is really working!</p>
-          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <button onClick={switchNameHandler}>Switch Name</button>
 
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+          <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+          <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
     // return React.createElement('div', null, React.createElement('h1', {className:'App'}, 'Hi I\'m a React App!'))
-  }
 }
 
-export default App;
+export default app;
+// state = {
+//   persons : [
+//     {name: "Simon", age: 48},
+//     {name: "Tarka", age: 1.5},
+//     {name: "Poacher", age: 45}
+//   ], otherState: 'some other value'
+// }
